aad2b6a63cb3915fb441ded507866b4d6d60cac2
cjluo <cjluo@stanford.edu>
2013-02-07 22:33:40 -0800

    trim down get-user and put-user

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -13,20 +13,9 @@
 #include "userprog/process.h"
 #include "userprog/pagedir.h"
 
-/* An open file. */
-struct file 
-  {
-    struct inode *inode;        /* File's inode. */
-    off_t pos;                  /* Current position. */
-    bool deny_write;            /* Has file_deny_write() been called? */
-  };
-
 static void syscall_handler (struct intr_frame *);
 
-static int get_user (const uint8_t *);
-static bool put_user (uint8_t *, uint8_t);
 static inline void check_valid_address (const void *);
-
 
 static int sys_halt (void);
 static int sys_exec (const char *);
@@ -334,30 +323,6 @@ sys_close (int fd)
     free (f);
   }
   return -1;
-}
-
-/* Reads a byte at user virtual address UADDR.
-UADDR must be below PHYS_BASE.
-Returns the byte value if successful, -1 if a segfault
-occurred. */
-static int
-get_user (const uint8_t *uaddr)
-{
-  int result;
-  asm ("movl $1f, %0; movzbl %1, %0; 1:"
-       : "=&a" (result) : "m" (*uaddr));
-  return result;
-}
-/* Writes BYTE to user address UDST.
-UDST must be below PHYS_BASE.
-Returns true if successful, false if a segfault occurred. */
-static bool
-put_user (uint8_t *udst, uint8_t byte)
-{
-  int error_code;
-  asm ("movl $1f, %0; movb %b2, %1; 1:"
-       : "=&a" (error_code), "=m" (*udst) : "q" (byte));
-  return error_code != -1;
 }
 
 static int
